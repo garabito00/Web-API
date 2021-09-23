f38b2f417098cbc8b3c2516905981a6cbac911df
garabito00 <91238183+garabito00@users.noreply.github.com>
2021-09-23 09:47:35 -0400

    Agregar comentarios al contexto.

--- Data/RopaContext.cs
+++ Data/RopaContext.cs
@@ -9,11 +9,13 @@ namespace WebApplication17.Data
 {
     public class RopaContext: DbContext
     {
+        //Inyeccion de dependencia de la conexion a base de datos
         public RopaContext(DbContextOptions<RopaContext> options): base(options)
         {
 
         }
-
+        
+        //DBset para crear la tabla en la base de datos
         public DbSet<Ropa> Ropas { get; set; }
     }
 }
