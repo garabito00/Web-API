460d15b0a7cd42348bae1439cfee8359e3b871e6
garabito00 <91238183+garabito00@users.noreply.github.com>
2021-09-23 09:52:49 -0400

    Agregar comentarios al repositorio Generico

--- Repositorio/GenericRepository.cs
+++ Repositorio/GenericRepository.cs
@@ -9,36 +9,42 @@ namespace WebApplication17.Repositorio
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : Models.Ropa
     {
+        //Inyeccion de dependencia del contexto para comunicar el repositorio con la base de datos.
         private readonly RopaContext _db;
         public GenericRepository(RopaContext db)
         {
             _db = db;
         }
+        
+        //Metodo para crear el registro en base de datos.
         public async Task Create(T model)
         {
             _db.Set<T>().Add(model);
            await _db.SaveChangesAsync();
         }
 
+        //Metodo para borrar un registro en la base de datos.
         public async Task Delete(T model)
         {
             _db.Set<T>().Remove(model);
             await _db.SaveChangesAsync();
         }
-
+        
+        //Metodo para traer todos los registros de la base de datos.
         public async Task<IEnumerable<T>> Read()
         {
             return await _db.Set<T>().ToListAsync();
         }
 
-
+        //Metodo para traer un solo registro de la base de datos
         public async Task<T> ReadOne(int id)
         {
             
             return await _db.Set<T>().FindAsync(id);
             
         }
-
+        
+        //Metodo para actualizar un registro en la base de datos.
         public async Task Update(T model)
         {
             _db.ChangeTracker.Clear();
